eef3c5d5ad10265b8635317258c09b0d68e17584
Luke Iannini <lukexi@me.com>
2015-12-15 05:12:41 -0800

    Implement in-world text editor

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,11 @@ import Cubensis
 main :: IO ()
 main = do
   cubensis $ \t -> 
-    [ newCube 
-        
-        { cubeColor = hslColor (x+z+t) 0.8 0.7 1 
-        , cubePosition = V3 x (sin (x + t) + sin (z + t)) z
-        , cubeRotation = axisAngle (V3 1 1 0) t
-        , cubeScale = 0.11 + realToFrac (sin t) * 0.1
+    [ newCube
+        { cubeColor = hslColor (x+z+t*1) 0.8 0.4 1 
+        , cubePosition = V3 (x*3) (sin (z + t*0.5) + sin (z + t)) (sin t * z-)
+        , cubeRotation = axisAngle (V3 0 0.5 0.5) (t*2)
+        , cubeScale = (V3 (0.11 + realToFrac (sin t) * 0.1) 0.33 (sin t))
         } 
       | x <- [-5, -4.8..5]
       , z <- [-5, -4.8..0]
--- src/Cubensis.hs
+++ src/Cubensis.hs
@@ -9,9 +9,12 @@ module Cubensis
 import Graphics.UI.GLFW.Pal as Exports
 import Graphics.VR.Pal as Exports
 import Graphics.GL.Pal as Exports
+import TinyRick
 
 import Control.Monad
 import Control.Monad.Reader
+import Control.Monad.State
+import Control.Concurrent
 import Halive.Utils
 
 data Cube = Cube
@@ -35,39 +38,67 @@ data Uniforms = Uniforms
   , uColor :: UniformLocation (V4 GLfloat) 
   } deriving Data
 
-cubensis :: (Fractional a) => (a -> [Cube]) -> IO ()
+cubensis :: (Real a, Fractional a) => (a -> [Cube]) -> IO ()
 cubensis getPositions = do
   vrPal@VRPal{..} <- reacquire 0 $ initVRPal "Cubensis" [UseOpenVR]
 
-  shader        <- createShaderProgram "src/geo.vert" "src/geo.frag"
+  shader        <- createShaderProgram "shaders/geo.vert" "shaders/geo.frag"
   uniforms      <- acquireUniforms shader
 
   cubeGeo       <- cubeGeometry 1 5
   cubeShape     <- (makeShape cubeGeo shader :: IO (Shape Uniforms))
 
+  glyphProg     <- createShaderProgram "shaders/glyph.vert" "shaders/glyph.frag"
+  font          <- createFont "fonts/SourceCodePro-Regular.ttf" 50 glyphProg
+
+  textMVar <- reacquire 1 $ newMVar =<< textRendererFromFile font "app/Main.hs"
+
   glEnable GL_DEPTH_TEST
+  glEnable GL_BLEND
+  glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA
   glClearColor 0.0 0.0 0.1 1
 
-  let player = Pose (V3 0 0 5) (axisAngle (V3 0 1 0) 0)
+  let player  = Pose (V3 0 0 5) (axisAngle (V3 0 1 0) 0)
+      textM44 = mkTransformation (axisAngle (V3 1 0 0) 0) (V3 0 0 4)
 
   start <- getNow
 
   whileVR vrPal $ \headM44 _hands -> do
-    processEvents gpEvents $ closeOnEscape gpWindow
+    player' <- execStateT (applyMouseLook gpWindow id) player
+
+    handleEvents vrPal textMVar textM44 player'
 
     -- Have time start at 0 seconds
     now <- (subtract start) <$> getNow
 
-    renderWith vrPal player headM44
+    text <- readMVar textMVar
+    renderWith vrPal player' headM44
       (glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT))
       $ \proj44 eyeView44 -> do
-          let projView = proj44 !*! eyeView44
+          let projViewM44 = proj44 !*! eyeView44
           withShape cubeShape $ 
-            render (uniforms :: Uniforms) projView (getPositions now)
+            renderCubes (uniforms :: Uniforms) projViewM44 (getPositions now)
+          let textMVP = projViewM44 !*! textM44
+          renderText text textMVP (V3 1 1 1)
 
-render :: (MonadIO m, MonadReader (Shape u) m) 
+
+handleEvents :: VRPal -> MVar TextRenderer -> M44 Float -> Pose Float -> IO ()
+handleEvents VRPal{..} textMVar textModelM44 playerPose = processEvents gpEvents $ \e -> do
+  closeOnEscape gpWindow e
+
+  modifyMVar textMVar $ \text -> do 
+    newText <- execStateT (handleTextBufferEvent gpWindow e id) text
+    return (newText, ())
+
+  onMouseDown e $ \_ -> modifyMVar textMVar $ \text -> do
+    winProj44 <- getWindowProjection gpWindow 45 0.1 1000
+    ray       <- cursorPosToWorldRay gpWindow winProj44 playerPose
+    newText   <- castRayToBuffer ray text textModelM44
+    return (newText, ())
+
+renderCubes :: (MonadIO m, MonadReader (Shape u) m) 
        => Uniforms -> M44 GLfloat -> [Cube] -> m ()
-render Uniforms{..} projView cubes = do
+renderCubes Uniforms{..} projView cubes = do
   forM_ cubes $ \Cube{..} -> do
     let model = mkTransformation cubeRotation cubePosition !*! scaleMatrix cubeScale
     uniformV4  uColor cubeColor
