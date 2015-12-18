7d8157c0fa2bc97dbe4d416cbbbfdd08f84e2fde
Luke Iannini <lukexi@me.com>
2015-12-17 18:45:11 -0800

    Wavy

--- defs/Cubes1.hs
+++ defs/Cubes1.hs
@@ -4,11 +4,10 @@ import Graphics.GL.Pal
 someCubes :: Float -> [Cube]
 someCubes = \t -> 
     [ newCube
-        { cubeColor = hslColor (x+z+t*1) 0.8 0.4 1 
-        , cubePosition = V3 (x*3) (sin (z + t*0.5) + sin (z + t)) (sin t * z)
-        , cubeRotation = axisAngle (V3 0 0.5 0.5) (t*2)
-        , cubeScale = (V3 (0.11 + realToFrac (sin t) * 0.1) 0.33 (sin t))
+        { cubeColor    = hslColor (x*0.01+t*0.3) 0.8 0.4 1 
+        , cubePosition = V3 (sin (t+x*0.11)) (x*0.1-1.5) (-11)
+        , cubeRotation = axisAngle (V3 0 0.5 0.5) t
+        , cubeScale    = V3 (0.05 + realToFrac (sin t) * 0.1) 0.1 (sin t*5)
         } 
-      | x <- [-5, -4.8..5]
-      , z <- [-5, -4.8..0]
+      | x <- [0..350]
     ]
